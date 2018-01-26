02dcf5aae9a7ff263d084eee2fbe00d3cf50397c
weakit <34541656+weakit@users.noreply.github.com>
2018-01-26 22:01:10 +0530

    Made AcoustID Optional
    + Cleaned up script

--- acoustID.py
+++ acoustID.py
@@ -7,10 +7,16 @@ import itertools
 import threading
 
 done = False
-animation = ['∙ ∙ ∙ ∙ ∙ ∙ ∙', ' ∙∙ ∙ ∙ ∙ ∙ ∙', '∙  ∙∙ ∙ ∙ ∙ ∙', '∙ ∙  ∙∙ ∙ ∙ ∙',
-             '∙ ∙ ∙  ∙∙ ∙ ∙', '∙ ∙ ∙ ∙  ∙∙ ∙', '∙ ∙ ∙ ∙ ∙  ∙∙', '∙ ∙ ∙ ∙ ∙ ∙ ∙',
-             '∙ ∙ ∙ ∙ ∙ ∙∙ ', '∙ ∙ ∙ ∙ ∙∙  ∙', '∙ ∙ ∙ ∙∙  ∙ ∙', '∙ ∙ ∙∙  ∙ ∙ ∙',
-             '∙ ∙∙  ∙ ∙ ∙ ∙', '∙∙  ∙ ∙ ∙ ∙ ∙']
+cmd = False
+# animation = ['∙ ∙ ∙ ∙ ∙ ∙ ∙', ' ∙∙ ∙ ∙ ∙ ∙ ∙', '∙  ∙∙ ∙ ∙ ∙ ∙', '∙ ∙  ∙∙ ∙ ∙ ∙',
+#              '∙ ∙ ∙  ∙∙ ∙ ∙', '∙ ∙ ∙ ∙  ∙∙ ∙', '∙ ∙ ∙ ∙ ∙  ∙∙', '∙ ∙ ∙ ∙ ∙ ∙ ∙',
+#              '∙ ∙ ∙ ∙ ∙ ∙∙ ', '∙ ∙ ∙ ∙ ∙∙  ∙', '∙ ∙ ∙ ∙∙  ∙ ∙', '∙ ∙ ∙∙  ∙ ∙ ∙',
+#              '∙ ∙∙  ∙ ∙ ∙ ∙', '∙∙  ∙ ∙ ∙ ∙ ∙']
+
+animation = ['.   ', '..  ', '... ', ' .. ', '  . ']
+
+# animation = ['⠁⠂⠄⡀⢀⠠⠐⠈', '⠂⠄⡀⢀⠠⠐⠈⠁', '⠄⡀⢀⠠⠐⠈⠁⠂', '⡀⢀⠠⠐⠈⠁⠂⠄',
+#              '⢀⠠⠐⠈⠁⠂⠄⡀', '⠠⠐⠈⠁⠂⠄⡀⢀', '⠐⠈⠁⠂⠄⡀⢀⠠', '⠈⠁⠂⠄⡀⢀⠠⠐']
 
 
 def animate():
@@ -19,11 +25,14 @@ def animate():
             break
         sys.stdout.write('\r'+an)
         sys.stdout.flush()
-        time.sleep(0.2)
+        time.sleep(0.1)
 
 
 base = "https://api.acoustid.org/v2/lookup"
-client = 'AcoustID API Key'
+client = 'bZoX2TyccH'  # AcoustID API Key
+# TODO Clean Vars
+M101 = os.path.join("C:/", "Users", "Administrator", "Music", "Maroon 5 - Red Pill Blues (Deluxe) (2017)", "102.mp3")
+# V102 = os.path.join("C:/", "Users", "Administrator", "Music", "Maroon 5 - Red Pill Blues (Deluxe) (2017)", "102.mp3")
 
 
 def Exit(reason):
@@ -32,10 +41,14 @@ def Exit(reason):
 
 
 def getprint(media):
+    global cmd
     finger = subprocess.run(['fpcalc', media], stdout=subprocess.PIPE)
     out = finger.stdout.decode('utf-8').split('\r\n')
     if out[0:5] == 'ERROR':
-        Exit(str('fpcalc error: '+out.split(': ')[1]))
+        if cmd:
+            Exit(str('fpcalc error: '+out.split(': ')[1]))
+        else:
+            return None
     duration = out[0].split('=')[1]
     fprint = out[1].split('=')[1]
     return [duration, fprint]
@@ -43,41 +56,56 @@ def getprint(media):
 
 def lookup(file):
     # check for an internet connection
+    global cmd
     while True:
         try:
             requests.get('https://acoustid.org/webservice')
             break
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            Exit("Can't reach AcoustID.")
+            if cmd:
+                Exit("Can't reach AcoustID.")
+            else:
+                return 'ERROR: NO INTERNET ACCESS'
     fingerprint = getprint(file)
+    if fingerprint is None:
+        return 'ERROR: FPCALC ERROR'
     lookup_id = base + "?client=" + client + '&duration=' + fingerprint[0] + '&fingerprint=' + fingerprint[1]
     raw_id = requests.get(lookup_id)
     if not raw_id.json()["status"] == 'ok':
-        Exit(str("Acoust ID Error: status " + raw_id.json()["status"]))
+        if cmd:
+            Exit(str("Acoust ID Error\n" + raw_id.json()["status"]))
+        else:
+            return 'ERROR: ACOUSTID ERROR'
     if len(raw_id.json()['results']) == 0:
-        Exit("No matches found.")
+        if cmd:
+            Exit("No matches found.")
+        else:
+            return 'ERROR: NO MATCHES FOUND'
     acoust_id = raw_id.json()["results"][0]["id"]
     score = float(raw_id.json()["results"][0]["score"]) * 100
     lookup_song = base + "?client=" + client + '&meta=recordings&trackid=' + acoust_id
     raw_song = requests.get(lookup_song)
     title = raw_song.json()['results'][0]['recordings'][0]['title']
-    mbz = raw_song.json()['results'][0]['recordings'][0]['id']
+    mbiz = raw_song.json()['results'][0]['recordings'][0]['id']
     artist = raw_song.json()['results'][0]['recordings'][0]['artists'][0]['name']
-    return [artist, title, mbz, score]
+    return [artist, title, mbiz, score]
 
-"""
-if not len(sys.argv) == 1:
-    if str(sys.argv[1]) == '-h' or str(sys.argv[1]) == '--help':
-        print("AcoustID Wrapper\nusage: acoustID.py file\n\nIdentifies the Title and Artist of <file> using AcoustID")
-        Exit("")
-    if os.path.isfile(str(sys.argv[1])):
-        t = threading.Thread(target=animate)
-        t.start()
-        details = lookup(sys.argv[1])
-        done = True
-        sys.stdout.write('\r' + ' ' * 15 + '\r')
-        print("'"+details[1] + "' by " + details[0])
-        print("https://www.musicbrainz.org/recording/"+details[2]+"\nPowered by AcoustID - https://acoustid.org/")
-    else:
-        print('File not found: '+sys.argv[1])
-"""
+
+if __name__ == '__main__':
+    if not len(sys.argv) == 1:
+        if str(sys.argv[1]) == '-h' or str(sys.argv[1]) == '--help':
+            print(
+                "AcoustID Wrapper\nusage: acoustID.py file\n\nIdentifies the Title and Artist of <file> using AcoustID")
+            Exit("")
+        if os.path.isfile(str(sys.argv[1])):
+            t = threading.Thread(target=animate)
+            t.start()
+            cmd = True
+            details = lookup(sys.argv[1])
+            done = True
+            sys.stdout.write('\r' + ' ' * 15 + '\r')
+            print("'" + details[1] + "' by " + details[0])
+            print(
+                "https://www.musicbrainz.org/recording/" + details[2] + "\nPowered by AcoustID - https://acoustid.org/")
+        else:
+            print('File not found: ' + sys.argv[1])
