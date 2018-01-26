900fd96e8319eb6fa455824187a686751529e05c
weakit <34541656+weakit@users.noreply.github.com>
2018-01-26 22:29:19 +0530

    Make AcoustID Optional
    + Clean Up Script

--- lyrical.py
+++ lyrical.py
@@ -2,40 +2,38 @@ import itertools
 import time
 import sys
 import threading
-import acoustID
 from search import handle, search
 import stagger
 from stagger.id3 import USLT
 import requests
 import os
 from bs4 import BeautifulSoup
-dots = acoustID.animation
-Fin = False
+
+dots = ['.   ', '..  ', '... ', ' .. ', '  . ']
+Fin = True
+printLyrics = False
 prefix = ''
 
 
 def animation():
     for frame in itertools.cycle(dots):
         if Fin:
-            break
+            time.sleep(0.1)
+            continue
         sys.stdout.write(' ' * 16 + '\r' + prefix + frame)
         sys.stdout.flush()
         time.sleep(0.1)
 
 
-t = threading.Thread(target=animation)
-t.start()
+thread = threading.Thread(target=animation)
+thread.daemon = True
+thread.start()
 
 base = 'https://genius.com/'
-# need ta figure out how regex works TODO Regex
 
 
-def Exit(reason):
-    print(reason)
-    exit(420)
-
-
-def get(url):
+def scrape(url):
+    """Scrapes Lyrics from URL"""
     while True:
         try:
             raw = requests.get(url)
@@ -43,10 +41,37 @@ def get(url):
             linguistics = soup.find('div', {'class': 'lyrics'}).get_text()
             return linguistics
         except AttributeError:
-            global Fin
+            global Fin, got
             Fin = True
             sys.stdout.write('\r')
-            return get(handle(search(artist + ' ' + title)))
+            return scrape(handle(search(got[0] + ' ' + got[1])))
+
+
+def read(file):
+    global prefix
+    prefix = 'Reading ID3 Tags  '
+    id3 = stagger.read_tag(file)
+    return id3.artist, id3.title
+
+
+def get(file):
+    """Gets the song artist and title from mp3"""
+    try:
+        import acoustID
+    except ModuleNotFoundError:
+        return read(file)
+    global prefix
+    prefix = 'Matching with AcoustID  '
+    aid = acoustID.lookup(path)
+    if str(aid).upper()[:5] == 'ERROR':
+        return read(file)
+    return aid[0], aid[1]
+
+
+def write(file, lyric):
+    tags = stagger.read_tag(file)
+    tags[USLT] = USLT(lang='eng', desc='Lyrics', text=lyric)
+    tags.write(path)
 
 
 try:
@@ -56,24 +81,18 @@ except IndexError:
     exit(69)
 
 if os.path.isfile(sys.argv[1]):
-    Fin = True
-    prefix = 'Matching with AcoustID  '
     Fin = False
-    ID = acoustID.lookup(path)
-    tags = stagger.read_tag(path)
-    if str(ID).upper()[:5] == 'ERROR':
-        print('AcoustID Error: ' + ID[7:] + '\n Using ID3 Tags Instead')
-        ID = [tags.artist, tags.title]
-    artist = ID[0]
-    title = ID[1]
+    got = get(path)
     prefix = 'Scraping Lyrics  '
-    genius = base+(artist+' '+title+' lyrics').replace(' ', '-').replace("'", '').replace('.', '')
-    lyrics = get(genius)
+    genius = base+('{0} {1} lyrics'.format(got[0], got[1]).replace(' ', '-').replace("'", '').replace('.', ''))
+    # need ta figure out how regex works
+    lyrics = scrape(genius)
+    prefix = 'Writing Lyrics to file  '
     Fin = True
-    sys.stdout.write('\r' + ' ' * 15 + '\r')
-    print(lyrics)
-    tags[USLT] = USLT(lang='eng', desc='Genius Lyrics', text=lyrics)
-    tags.write(path)
+    write(path, lyrics)
+    sys.stdout.write('\r' + ' ' * 15 + '\r' + '\nScraped Lyrics.')
+    if printLyrics:
+        print('\n\n' + lyrics)
 else:
     print(str(path) + ' is not a valid file.')
     exit(420)
